f31a7a379fc8f865eba45cec03ac255bb54628ad
sunnystockdetect <65324317+sunnystockdetect@users.noreply.github.com>
2020-05-15 06:52:14 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-#載入LineBot所需要的套件
-from flask import Flask, request, abort
+# -*- coding: utf-8 -*-
+''''
+Created on Sat Aug 18 01:00:17 2018
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+@author: linzino
+'''
+from flask import Flask, request, abort
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
+import mongodb
+import re
 
 app = Flask(__name__)
 
@@ -15,7 +17,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('fkZZVXaAF/e48XU6uQ5m/Ma1UdPVo2Cz7s+risWsSmh4NyMUGj0OqzxPWfoq02jah1VQa+9uZTUWDWP/ItVz2ILXr8EaKACOM/XttyexVjZl8XP4us8yztBS//D+PHai6iyDoJk/nTx/2RSuxO9yAAdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('1c947d2476365222ae038aebe1b202c0')
-# 必須放上自己的Your user ID
+# 必須放上自己的You User ID
 line_bot_api.push_message('U53b88e7039478edcee8eef5ae6c72142', TextSendMessage(text='你可以開始了'))
 
 # 監聽所有來自 /callback 的 Post Request
@@ -33,17 +35,25 @@ def callback():
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
 #訊息傳遞區塊
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    ### 抓到顧客的資料 ###
+    profile = line_bot_api.get_profile(event.source.user_id)
+    uid = profile.user_id #使用者ID
+    usespeak=str(event.message.text) #使用者講的話
+    if re.match('[0-9]{4}[<>][0-9]',usespeak): # 先判斷是否是使用者要用來存股票的
+        mongodb.write_user_stock_fountion(stock=usespeak[0:4], bs=usespeak[4:5], price=usespeak[5:])
+        #line_bot_api.push_message(uid, TextSendMessage(usespeak[0:4]+'已經儲存成功'))
+        line_bot_api.push_message('U53b88e7039478edcee8eef5ae6c72142', TextSendMessage(usespeak[0:4]+'已經儲存成功'+uid))
+        return 0    
+    elif re.match('刪除[0-9]{4}',usespeak): # 刪除存在資料庫裡面的股票
+        mongodb.delete_user_stock_fountion(stock=usespeak[2:])
+        #line_bot_api.push_message(uid, TextSendMessage(usespeak+'已經刪除成功'))
+        line_bot_api.push_message('U53b88e7039478edcee8eef5ae6c72142', TextSendMessage(usespeak[0:4]+'已經刪除成功'+uid))
+        return 0
 
-#主程式
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
