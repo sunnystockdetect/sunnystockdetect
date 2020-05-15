052a3b75724da4cc1d6c94cf734508b289600a23
sunnystockdetect <65324317+sunnystockdetect@users.noreply.github.com>
2020-05-15 12:30:00 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -34,6 +34,21 @@ def callback():
         abort(400)
     return 'OK'
 
+@handler.add(JoinEvent)
+def handle_join(event):
+    newcoming_text = "謝謝邀請我這個機器來至此群組！！我會盡力為大家服務的～"
+
+    line_bot_api.reply_message(
+            event.reply_token,
+            TextMessage(text=newcoming_text)
+        )
+    print("JoinEvent =", JoinEvent)
+
+@handler.add(LeaveEvent)
+def handle_leave(event):
+    print("leave Event =", event)
+    print("我被踢掉了QQ 相關資訊", event.source)
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
