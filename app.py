6fe701de9806bc42180b4099f2a19297b4fecafb
sunnystockdetect <65324317+sunnystockdetect@users.noreply.github.com>
2020-05-15 14:08:01 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def callback():
 
 @handler.add(JoinEvent)
 def handle_join(event):
-    newcoming_text = "謝謝邀請我這個機器來至此群組！！我會盡力為大家服務的～"
+    newcoming_text = "謝謝邀請「晴股偵測儀」來至此群組！！我會盡力為大家服務的～"
 
     line_bot_api.reply_message(
             event.reply_token,
