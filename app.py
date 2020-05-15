a913a7b4a37d2735531459bf3216c7a01f0824b8
sunnystockdetect <65324317+sunnystockdetect@users.noreply.github.com>
2020-05-15 21:04:28 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-import mongodb
+from pymongo import MongoClient
+import urllib.parse
+import datetime
 import re
 
 app = Flask(__name__)
@@ -19,6 +21,36 @@ handler = WebhookHandler('1c947d2476365222ae038aebe1b202c0')
 # 必須放上自己的You User ID (PUSH指令要收費)
 #line_bot_api.push_message('U53b88e7039478edcee8eef5ae6c72142', TextSendMessage(text='您好!我已準備提供服務...'))
 
+##### 資料庫連接 #####
+def constructor():
+    client = MongoClient('mongodb+srv://root:rootjimmystock313@cluster0-racxf.gcp.mongodb.net/test?retryWrites=true&w=majority')
+    db = client['sunnystockdb']
+    return db
+   
+#----------------------------儲存使用者的股票--------------------------
+def write_user_stock_fountion(stock, bs, price):  
+    db=constructor()
+    collect = db['mydb']
+    collect.insert({"stock": stock,
+                    "data": 'care_stock',
+                    "bs": bs,
+                    "price": float(price),
+                    "date_info": datetime.datetime.utcnow()
+                    })
+    
+#----------------------------殺掉使用者的股票--------------------------
+def delete_user_stock_fountion(stock): 
+    db=constructor()
+    collect = db['mydb']
+    collect.remove({"stock": stock})
+    
+#----------------------------秀出使用者的股票--------------------------
+def show_user_stock_fountion():  
+    db=constructor()
+    collect = db['mydb']
+    cel=list(collect.find({"data": 'care_stock'}))
+
+    return cel
 
 
 # 監聽所有來自 /callback 的 Post Request
