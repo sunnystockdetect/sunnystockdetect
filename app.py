6ed9639ebe9112f8895fa5610fc7cb32cad2edc2
sunnystockdetect <65324317+sunnystockdetect@users.noreply.github.com>
2020-05-15 09:32:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -39,10 +39,19 @@ def handle_message(event):
     #message = TextSendMessage(text=event.message.text)
 
     #ImageSendMessage（圖片訊息）
+    '''
     message = ImageSendMessage(
         original_content_url='https://sunnystockdetect.herokuapp.com/callback/YellowSpyder.jpg',
         preview_image_url='https://sunnystockdetect.herokuapp.com/callback/YellowSpyder.jpg'
+    '''
 
+    #LocationSendMessage（位置訊息）
+    message = ImageSendMessage(
+        title='my location',
+        address='Tokyo',
+        latitude=35.65910807942215,
+        longitude=139.70372892916203
+    )
     line_bot_api.reply_message(event.reply_token, message)
 '
 )
