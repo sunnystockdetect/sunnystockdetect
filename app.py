2b7e21109429b78267b4bba9fa049135a4e1684b
sunnystockdetect <65324317+sunnystockdetect@users.noreply.github.com>
2020-05-15 09:45:37 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -55,8 +55,7 @@ def handle_message(event):
     )
     '''
     line_bot_api.reply_message(event.reply_token, message)
-'
-)
+
 line_bot_api.reply_message(event.reply_token, message)
 import os
 if __name__ == "__main__":
