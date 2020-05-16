ffbee11abd7021c4e8081caa3f54b82d9a699344
sunnystockdetect <65324317+sunnystockdetect@users.noreply.github.com>
2020-05-16 12:15:48 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -61,11 +61,11 @@ def show_user_stock_fountion():
 
     return cel
 
-
+'''
 @app.route("/")
 def home():
     return render_template("home.html")
-
+'''
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
